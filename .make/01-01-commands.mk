2c64a1c6c93734d867cc8cb428ce35ec754d1207
Asapdotid <asapdotid@gmail.com>
2024-12-19 18:24:35 +0700

    ♻️ chore: fix script

--- .make/01-01-commands.mk
+++ .make/01-01-commands.mk
@@ -1,12 +1,11 @@
 ##@ [Utility Commands]
 
 # @see https://stackoverflow.com/a/43076457
-.PHONY: execute-in-container
-execute-in-container: ## Execute a command in a container. E.g. via "make execute-in-container DOCKER_SERVICE_NAME=traefik COMMAND="echo 'hello'"
+execute-in-container:
 	@$(if $(DOCKER_SERVICE_NAME),,$(error DOCKER_SERVICE_NAME is undefined))
 	@$(if $(COMMAND),,$(error COMMAND is undefined))
 	@$(EXECUTE_IN_ANY_CONTAINER) $(COMMAND)
 
 .PHONY: shell
-shell-master: ## Execute shell in Adminer container with arguments ARGS="pwd"
+shell: ## Execute shell in Adminer container with arguments ARGS="pwd"
 	@$(EXECUTE_IN_APPLICATION_CONTAINER) $(ARGS);
